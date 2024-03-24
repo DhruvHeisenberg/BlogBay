0fd5f64ec65becc580a89fbd10dc451069ffe498
The1WhoKnocks <97444040+DhruvHeisenberg@users.noreply.github.com>
2024-03-25 01:46:30 +0530

    Update s3.js

--- api/s3.js
+++ api/s3.js
@@ -6,8 +6,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner")
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const accessKeyId = process.env.ACCESS_KEY
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
 
 const s3Client = new S3Client({
   region,
