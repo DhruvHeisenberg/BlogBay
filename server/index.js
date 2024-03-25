04074466bdedf122db988f8ca84cc9693db70f26
DhruvHeisenberg <dhruvtripathi.7777@gmail.com>
2024-03-25 05:36:55 +0530

    commit06

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const { uploadFile, deleteFile, getObjectSignedUrl } = require('./s3.js')
 app.use(cors({
     origin: ["*"],
     methods: ["*"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
 }))
 
