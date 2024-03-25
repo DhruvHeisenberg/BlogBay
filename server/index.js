f5320e4c55535a07292edf2ee1df643070d2d8b2
DhruvHeisenberg <dhruvtripathi.7777@gmail.com>
2024-03-25 20:34:45 +0530

    commit13

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
